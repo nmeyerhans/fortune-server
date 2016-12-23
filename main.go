b06f6db728b0e3ed2fb13361858d01e3a3fec1cc
Noah Meyerhans <nmeyerha@amazon.com>
2016-12-23 14:21:01 -0800

    Add logging

--- main.go
+++ main.go
@@ -12,27 +12,43 @@ type Fortune struct {
 	Message string
 }
 
+func logRequest(req *http.Request, responseCode int, responseSize int) {
+	log.Printf("%s %s%s %d %d\n", req.RemoteAddr, req.Host, req.RequestURI, responseCode, responseSize)
+}
+
 func serveHealthcheck(w http.ResponseWriter, req *http.Request) {
+	responseSize := 0
+	responseCode := http.StatusOK
+	defer func() {
+		logRequest(req, responseCode, responseSize)
+	}()
 	if !fortune.Available() {
-		w.WriteHeader(http.StatusInternalServerError)
+		responseCode = http.StatusInternalServerError
 	}
-	w.WriteHeader(http.StatusOK)
 }
 
 func serveFortune(w http.ResponseWriter, req *http.Request) {
+	responseSize := 0
+	responseCode := http.StatusOK
+	defer func() {
+		logRequest(req, responseCode, responseSize)
+	}()
 	f, err := fortune.Fortune(true)
 	if err != nil {
+		responseCode = http.StatusInternalServerError
 		log.Fatal(err)
 	}
 	m := Fortune{f}
 	b, err := json.Marshal(m)
 	if(err != nil) {
+		responseCode = http.StatusInternalServerError
 		log.Fatal(err)
 	}
 	if req.Header.Get("UserAgent") != "" {
 		fmt.Printf("Got a request from a %s browser\n", req.Header.Get("UserAgent"))
 	}
 	// fmt.Print(string(b))
+	responseSize = len(b)
 	w.Write(b)
 }
 
